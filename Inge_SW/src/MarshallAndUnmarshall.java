1cb942e0bb86f17b40571f62e865429bff50a49a
Alberto <albertomarini97@gmail.com>
2022-01-23 11:17:17 +0100

    Lavoro parziale su marshall

--- Inge_SW/src/MarshallAndUnmarshall.java
+++ Inge_SW/src/MarshallAndUnmarshall.java
@@ -4,9 +4,11 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamReader;
 import java.io.File;
+import java.io.FileInputStream;
 
 public class MarshallAndUnmarshall {
 
@@ -16,7 +18,11 @@ public class MarshallAndUnmarshall {
     private final String xmlNet = "Savings.xml";
     private final String xmlProva = "TEST1_CONVERSIONE.xml";
 
+    //Xml Schema da provare
     private final String xsdNet = "savings.xsd";
+
+    XMLInputFactory xmlif = null;
+    XMLStreamReader xmlr = null;
 
     //MARSHALL = SCRITTURA
 
@@ -63,8 +69,9 @@ public class MarshallAndUnmarshall {
 
         Global proofOfWork = (Global) unmarshaller.unmarshal(new File(xmlNet));
         System.out.println("\t---\nUnmarshall eseguito\n\t---");
-       // System.out.println(proofOfWork.toString());
+        // System.out.println(proofOfWork.toString());
     }
+
     public Global unmarshallReturn() throws JAXBException {
 
         //SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
@@ -81,4 +88,34 @@ public class MarshallAndUnmarshall {
         //System.out.println(proofOfWork.toString());
         return proofOfWork;
     }
+
+    public void initReader() {
+
+        try {
+            xmlif = XMLInputFactory.newInstance();
+            xmlr = xmlif.createXMLStreamReader(new FileInputStream(xmlNet));
+        } catch (Exception e) {
+            System.out.println("Errore nell'inizializzazione del reader:");
+        }
+    }
+
+    public void readXml() {
+        try {
+            while (xmlr.hasNext()) {
+                if (xmlr.getEventType() == XMLStreamConstants.START_ELEMENT) {
+
+                    for (int i = 0; i < xmlr.getAttributeCount(); i++) {
+                        //da vedere con andrea
+                    }
+                } else if ()
+
+            }
+            xmlr.next();
+
+        } catch (Exception e) {
+            System.out.println("Errore nella lettura");
+        }
+    }
+
+
 }
